0975012afff805ba2ee65c4e09dc8c5781a8f046
eyadfattah23 <eyadabdelfattah4@gmail.com>
2022-12-23 19:33:19 +0200

    kk

---
+++ eyadio.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+using namespace std;
+
+void fastio()
+{
+   ios_base::sync_with_stdio(false);
+cin.tie(NULL); 
+cout.tie(NULL); 
+}
+
+int main(){
+    fastio();
+
+
+
+return 0;}
